49538bc36b9550f61e964b0cf6869dc2b51be352
zadean <contact@zadean.com>
2019-01-02 11:32:05 +0100

    Bump year

--- lib/xqerl/src/xqerl_restxq.erl
+++ lib/xqerl/src/xqerl_restxq.erl
@@ -2,7 +2,7 @@
 %%
 %% xqerl - XQuery processor
 %%
-%% Copyright (c) 2018 Zachary N. Dean  All Rights Reserved.
+%% Copyright (c) 2018-2019 Zachary N. Dean  All Rights Reserved.
 %%
 %% This file is provided to you under the Apache License,
 %% Version 2.0 (the "License"); you may not use this file
